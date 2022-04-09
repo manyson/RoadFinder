55fb9e5c69f2d5da00b33263467e765c6d71d2af
iu00 <q1w2e3>
2022-04-09 21:07:13 +0900

    Initial commit

---
+++ src/main/java/com/manyson/road/RoadFinderMain.java
@@ -0,0 +1,17 @@
+package com.manyson.road;
+
+/**
+ * 도로명을 추출하는 프로그램의 메인 클래스
+ *
+ * @author  김정현
+ * @version 1.0
+ */
+public class RoadFinderMain {
+
+    /**
+     * 도로명을 추출하는 프로그램의 메인 메소드
+     * @param args 프로그램 실행 argument
+     */
+    public static void main(String[] args) {
+    }
+}
